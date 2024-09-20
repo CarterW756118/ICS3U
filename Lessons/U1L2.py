a4a216384460e600cc66a8ff37ed43bdf65bbc49
Carter <756118@pdsb.net>
2024-09-20 09:08:07 -0400

    Update U1L2.py

--- Lessons/U1L2.py
+++ Lessons/U1L2.py
@@ -9,6 +9,12 @@ print("Two dozen is: ", amount)
 # Modify 2:
 amount = 7 * 4
 print("forty-two is: ", amount)
+# The fixed output would be
+amount = 7 * 4
+print("twenty-eight is: ", amount)
+# or
+amount = 7 * 6
+print("forty-two is: ", amount)
 # Modify 3:
 amount = 8
 amount += 1
