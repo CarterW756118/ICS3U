ec653900e3a26747738cdfbfafde085813c9e542
Carter <756118@pdsb.net>
2024-10-31 10:09:16 -0400

    Update U3L11.py

--- Lessons/U3L11.py
+++ Lessons/U3L11.py
@@ -2,16 +2,16 @@ import random
 
 def shuffle(A):
     for i in range(len(A)):
-      temp = B[i]
-      j = random.randint(0, len(A) - 1)
-      B[i] = B[j]
-      B[j] = temp
+        temp = A[i]
+        j = random.randint(0, len(A) - 1)
+        A[i] = A[j]
+        A[j] = temp
     return A
     
 size = 6
 B = []
 for i in range(1, size + 1):
-   B.append(i)
+    B.append(i)
 
 print(B) # before
 B = shuffle(B)
