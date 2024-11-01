7ed033789fbf7c593a0118a754b28ddacf89b637
Carter <756118@pdsb.net>
2024-11-01 10:10:48 -0400

    Add files via upload

--- school_yearbook.py
+++ school_yearbook.py
@@ -6,7 +6,8 @@ Program : School Yearbook
 Description : A program that computes the minimum area and dimensions for photos with 
     1x1 units being placed in a yearbook.
 VARIABLE DICTIONARY :
-    done (bool) = Boolean value of if the user has entered done and if the loop should keep running
+    done (bool) = Boolean value of if the user has entered done and if the loop should
+        keep running
     valid (bool) = Boolean value of if the user has entered valid input
     num_photos (int) = Number of photos the user has input
     user_input (str) = User input for the amount of photos or "done"
@@ -23,11 +24,13 @@ import math
 
 # Finds factors of a number and returns them as a list
 def factor(N):
+    # Defines empty factors list
     factors = []
-    # Max num/factor
+    # Max num/factor, which is the rounded down value of the square root of N
     max_num = math.floor(math.sqrt(N))
-    # Finds the factors of N, and appends them to factors list
+    # Finds the factors of N, and appends/adds them to factors list
     for x in range(1, max_num + 1):
+        # If N divided by x has no remainder, add x to the factors list
         if N % x == 0:
             factors.append(x)
     # Return factors list
@@ -40,6 +43,7 @@ def min_perimeter(N):
     x = 1
     # Finds the max value from the factor list
     for num in factors:
+        # If num is greater then x, set x to num
         if num > x:
             x = num
     # Sets y to N divided by x
@@ -49,17 +53,20 @@ def min_perimeter(N):
     # Prints perimeter and the demensions
     print("Minimum perimeter is %d with demensions of %d x %d" % (perimeter, x, y))
 
+# Prints welcome message
 print("Welcome to the school yearbook program!")
 done = False
 # Runs while done is false (until user inputs "done")
 while not done:
+    # Prints a new line
     print()
     valid = False
     num_photos = 0
     # Runs until user input is considered valid
     while not valid:
+        # Asks user for number of photograpghs
         user_input = input("Please input your number of photograpghs: ")
-        # If the user input is "done", set done to false and exit the loop
+        # If the user input is "done", set done and valid to true and exit the loop
         if user_input.lower() == "done":
             valid = True
             done = True
@@ -74,9 +81,11 @@ while not done:
                 num_photos = user_input_int
             # User input number number less than 1
             else:
+                # Prints that the input is not a valid number of photos
                 print("%d is not a valid number of photos." % user_input_int)
         # Runs if user_input could not be converted into int, meaning user input a string
         except:
+            # Prints that the input is not a valid number of photos
             print("%s is not a valid number of photos." % user_input)
     # Calls min_perimeter function if done is false (so it does not run if user input "done" before)
     if not done:
