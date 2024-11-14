9b5feaeffba4f64bfb5056027a63888dc4b49540
Carter <756118@pdsb.net>
2024-11-14 12:24:56 -0500

    Add files via upload

--- Lessons/U5L2.py
+++ Lessons/U5L2.py
@@ -1,10 +1,9 @@
-filename = "smiley_emoji_mod.xpm"
+filename = "C://Users/Carter/Downloads/smiley_emoji_mod.xpm"
 fh = open(filename, "r")
 
 colorData = fh.readline()
 colorData = colorData.strip()
-colorData = colorData.replace('"', '')
-colorData = colorData.replace(',', '')
+colorData = colorData.replace('"', '').replace(',', '')
 rows, cols, numColors = colorData.split()
 
 rows = int(rows)
@@ -15,17 +14,30 @@ colorDefs = []
 for i in range(numColors):
     colorLine = fh.readline() 
     colorLine = colorLine.strip()
-    colorLine = colorLine.replace('"', '')
-    colorLine = colorLine.replace(',', '')
+    colorLine = colorLine.replace('"', '').replace(',', '')
     sym, c, color = colorLine.split()
     if sym == '~':
         sym = " "
-    colorArr = []
-    colorArr.append(sym)
-    colorArr.append(color)
-    colorDefs.append(colorArr)
+    colorDefs.append([sym, color])
+
+imageData = []
+for i in range(rows):
+    row = fh.readline()
+    row = row.strip()
+    row = row.replace('"', '').replace(',', '')
+    rowArr = []
+    for j in range(len(row)):
+        color = row[j]
+        for k in range(numColors):
+            if color == colorDefs[k][0]:
+                color = colorDefs[k][1]
+        rowArr.append(color)
+    imageData.append(rowArr)
 
 print("Dimensions: %d x %d" % (rows, cols))
+print("Number of colors:", numColors)
 print("Colors:", colorDefs)
+for i in imageData:
+    print(i)
 
 fh.close()
